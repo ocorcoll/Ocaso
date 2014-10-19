421cff986ad4ad9664a31c3dffe5c78f605a27ac
Oriol Corcoll <ocorcoll@gmail.com>
2014-10-19 20:10:51 +0300

    Test BIOS print routine

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,6 +1,23 @@
-loop:
-    jmp loop
+; Sets the BIOS routine
+mov ah,0x0e
 
+; Print characters:
+mov al,'O'
+int 0x10
+mov al,'r'
+int 0x10
+mov al,'i'
+int 0x10
+mov al,'o'
+int 0x10
+mov al,'l'
+int 0x10
+
+; Jump forever
+jmp $
+
+; Padding of 0s
 times 510-($-$$) db 0
 
+; Boot sector
 dw 0xaa55
